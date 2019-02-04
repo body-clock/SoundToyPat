c29b30d4f07e18489ce8126b0333befe0c3b96ed
Patrick Perkins <plp248@nyu.edu>
2019-02-03 19:08:44 -0500

    initial commit

---
+++ SoundToyBasic/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+	public Transform model;
+	public float rate = 1.0f;
+
+	float progress = 0.0f;
+
+	void Spawn()
+	{
+		Transform next = Instantiate(model, transform.position, transform.rotation) as Transform;
+		next.parent = transform;
+		next.localPosition = Vector3.zero;
+	}
+
+	void Update()
+	{
+		progress += rate * Time.deltaTime;
+		if (progress >= 1.0f)
+		{
+			Spawn();
+			progress -= 1.0f;
+		}
+	}
+}
